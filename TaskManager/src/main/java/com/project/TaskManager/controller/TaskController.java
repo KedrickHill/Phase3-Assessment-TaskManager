7d36f722632095b36539cc3959b6bf6c707b5774
KedrickHill <kedrickjhill@gmail.com>
2021-03-25 10:02:54 -0500

    Added base controller

---
+++ TaskManager/src/main/java/com/project/TaskManager/controller/TaskController.java
@@ -0,0 +1,15 @@
+package com.project.TaskManager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TaskController {
+
+	@GetMapping("/")
+	public String main() {
+		return "index";
+	}
+	
+	
+}
