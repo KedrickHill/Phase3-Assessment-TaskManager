e35a36f68c67a00a283928d41d3b68b7dbf50264
KedrickHill <kedrickjhill@gmail.com>
2021-03-25 14:58:48 -0500

    Switched which is being used

--- TaskManager/src/main/java/com/project/TaskManager/controller/MvcConfig.java
+++ TaskManager/src/main/java/com/project/TaskManager/controller/MvcConfig.java
@@ -1,14 +1,12 @@
 package com.project.TaskManager.controller;
 
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class MvcConfig implements WebMvcConfigurer{
-
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/index").setViewName("index");
-		registry.addViewController("/").setViewName("index");
-		registry.addViewController("/login").setViewName("login");
-		registry.addViewController("/manager").setViewName("manager");
-	}
+//
+//	public void addViewControllers(ViewControllerRegistry registry) {
+//		registry.addViewController("/").setViewName("login");
+//		registry.addViewController("/login").setViewName("login");
+//		registry.addViewController("/manager").setViewName("manager");
+//	}
 }
--- TaskManager/src/main/java/com/project/TaskManager/controller/TaskController.java
+++ TaskManager/src/main/java/com/project/TaskManager/controller/TaskController.java
@@ -2,13 +2,22 @@ package com.project.TaskManager.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TaskController {
 
-	/*
-	 * @GetMapping("/") public String main() { return "index"; }
-	 * 
-	 * @GetMapping("/login")
-	 */
+	@GetMapping({ "/", "/login"})
+	public ModelAndView login() {
+		ModelAndView model = new ModelAndView();
+		model.setViewName("login");
+		return model;
+	}
+
+	@GetMapping("/user-main")
+	public ModelAndView Main() {
+		ModelAndView model = new ModelAndView();
+		model.setViewName("user-main");
+		return model;
+	}
 }
