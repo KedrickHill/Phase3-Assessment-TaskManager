2f0b60ac9f30f6b022835470c148fb6121cafbc4
KedrickHill <kedrickjhill@gmail.com>
2021-03-25 10:23:47 -0500

    Added app properties and setup configurer while commenting out controller

---
+++ TaskManager/src/main/java/com/project/TaskManager/controller/MvcConfig.java
@@ -0,0 +1,14 @@
+package com.project.TaskManager.controller;
+
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MvcConfig implements WebMvcConfigurer{
+
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/index").setViewName("index");
+		registry.addViewController("/").setViewName("index");
+		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/manager").setViewName("manager");
+	}
+}
